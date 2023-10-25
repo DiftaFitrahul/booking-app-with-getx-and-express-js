aff73a29121d955cb9d75789164c93f64b272091
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-10-25 21:26:33 +0700

    feat: finish custom bottom navigation

---
+++ lib/view/pages/home/bottom_navigation_bar.dart
@@ -0,0 +1,108 @@
+import 'package:booking_app/view/constants/fonts/air_bnb_cereal.dart';
+import 'package:booking_app/view/pages/home/bottom_navigation_item.dart';
+import 'package:flutter/material.dart';
+
+class FABBottomAppBar extends StatefulWidget {
+  FABBottomAppBar({
+    super.key,
+    required this.items,
+    this.centerItemText = 'Add',
+    this.height = 70.0,
+    this.iconSize = 27.0,
+    this.notchedShape = const CircularNotchedRectangle(),
+    required this.backgroundColor,
+    required this.color,
+    required this.selectedColor,
+    required this.onTabSelected,
+  }) {
+    assert(items.length == 2 || items.length == 4);
+  }
+  final List<FABBottomAppBarItem> items;
+  final String? centerItemText;
+  final double height;
+  final double iconSize;
+  final Color backgroundColor;
+  final Color color;
+  final NotchedShape? notchedShape;
+  final Color selectedColor;
+
+  final ValueChanged<int> onTabSelected;
+
+  @override
+  State<StatefulWidget> createState() => FABBottomAppBarState();
+}
+
+class FABBottomAppBarState extends State<FABBottomAppBar> {
+  int _selectedIndex = 0;
+
+  _updateIndex(int index) {
+    widget.onTabSelected(index);
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> items = List.generate(widget.items.length, (int index) {
+      return _buildTabItem(
+        item: widget.items[index],
+        index: index,
+        onPressed: _updateIndex,
+      );
+    });
+
+    items.insert(items.length >> 1, _buildMiddleTabItem());
+
+    return BottomAppBar(
+      shape: widget.notchedShape,
+      color: widget.backgroundColor,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: items,
+      ),
+    );
+  }
+
+  Widget _buildMiddleTabItem() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40.0),
+    );
+  }
+
+  Widget _buildTabItem({
+    required FABBottomAppBarItem item,
+    required int index,
+    required ValueChanged<int> onPressed,
+  }) {
+    Color color = _selectedIndex == index ? widget.selectedColor : widget.color;
+    return Expanded(
+      child: SizedBox(
+        height: widget.height,
+        child: InkWell(
+          splashColor: Colors.transparent,
+          onTap: () => onPressed(index),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              Icon(item.iconData, color: color, size: widget.iconSize),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                item.text,
+                style: TextStyle(
+                    color: color, fontFamily: airBnbCereal, fontSize: 12),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/pages/home/home.dart
+++ lib/view/pages/home/home.dart
@@ -1,13 +1,26 @@
 import 'package:booking_app/model/repository/home/event.dart';
 import 'package:booking_app/view/components/home/header_event_title.dart';
+import 'package:booking_app/view/constants/fonts/air_bnb_cereal.dart';
+import 'package:booking_app/view/pages/home/bottom_navigation_bar.dart';
+import 'package:booking_app/view/pages/home/bottom_navigation_item.dart';
 import 'package:booking_app/view/pages/home/bottom_padding.dart';
 import 'package:booking_app/view/pages/home/header_sliver_custom.dart';
 import 'package:booking_app/view/pages/home/invite_friend_card.dart';
 import 'package:booking_app/view/pages/home/list_event_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _selectedTab(int index) {
+    print(index);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +47,52 @@ class HomePage extends StatelessWidget {
           const ListEventCard(data: EventRepository.nearbyYou),
           const BottomHomePadding()
         ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: SizedBox(
+        height: 70,
+        width: 70,
+        child: FloatingActionButton(
+          backgroundColor: const Color(0xFF5669FF),
+          onPressed: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xFF5669FF),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(100),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFF5669FF).withOpacity(0.3),
+                  spreadRadius: 9,
+                  blurRadius: 9,
+                  offset: const Offset(0, 8),
+                ),
+              ],
+            ),
+            child: const Icon(
+              Icons.add_box,
+              size: 33,
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: FABBottomAppBar(
+        backgroundColor: Colors.white,
+        centerItemText: null,
+        notchedShape: null,
+        color: Colors.grey,
+        selectedColor: const Color(0xFF5669FF),
+        onTabSelected: _selectedTab,
+        items: [
+          FABBottomAppBarItem(
+              iconData: CupertinoIcons.compass_fill, text: 'Explore'),
+          FABBottomAppBarItem(
+              iconData: Icons.event_note_outlined, text: 'Events'),
+          FABBottomAppBarItem(iconData: Icons.pin_drop_sharp, text: 'Map'),
+          FABBottomAppBarItem(
+              iconData: CupertinoIcons.person_fill, text: 'Profile'),
+        ],
       ),
     );
   }
